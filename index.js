1e9b00fd6c54c3eb5352a5bde1d3ed77564d5278
GracjanPW <40919569+GracjanPW@users.noreply.github.com>
2021-05-29 19:45:01 +0100

    01 api key hidden

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -46,4 +47,4 @@ client.on('message',msg=>{
     } 
 })
 
-client.login('ODQ3OTQwNDg1MDUzOTM5NzU0.YLFYHQ.a3IyiMlmGYdof55tuG6XpFobwvg');
+client.login(process.env.API_KEY);
